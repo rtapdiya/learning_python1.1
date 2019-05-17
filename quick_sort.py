9df7456f94b6b985bb7819af1572a3dd70291bc0
rtapdiya <tapdiya.rekha@gmail.com>
2019-05-17 10:55:06 -0700

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -19,7 +19,7 @@ def find_pivot(input_list,left_index, right_index):
         while input_list[left_index] <= pivot:
             left_index += 1
         while input_list[right_index] >= pivot:
-            right_index += 1
+            right_index -= 1
         input_list[left_index], input_list[right_index] = input_list[right_index], input_list[left_index]
     input_list[pivot_index],input_list[right_index] = input_list[right_index], input_list[pivot_index]
     return right_index
